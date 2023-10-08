70097813dfa57c75a2b40c7acfae2271c1aa9e05
slightscan <tohohh1@gmail.com>
2023-10-08 08:33:51 +0700

    commit

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
 export * from "./c4/c4FindingsParser.js";
 export * from "./sherlock/sherlockFindingsParser.js";
 export * from "./util.js";
+export * from "./types.js";
+export * from "./config.js";
